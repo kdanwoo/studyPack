29766a94213cc5155b32679fcf168da31b6814aa
kdanwoo <k.danwoo91@gmail.com>
2021-05-30 14:07:48 +0900

    자바 초기화 예제

---
+++ src/initailize/InitializationTest.java
@@ -0,0 +1,20 @@
+package initailize;
+
+public class InitializationTest {
+
+    int val2;
+    String val3;
+    byte val4;
+    boolean val5;
+
+    public static void main(String[] args) {
+        //객체 생성
+        InitializationTest test = new InitializationTest();
+        int val=1;
+        System.out.println(val);
+        System.out.println(test.val2);
+        System.out.println(test.val3);
+        System.out.println(test.val4);
+        System.out.println(test.val5);
+    }
+}
